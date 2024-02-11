d422798ea64aad79f10d22ab6fae047ca09d8d41
JuanObiJuan <obi_juan_@hotmail.com>
2024-02-11 13:03:31 +0100

    Allow users to login logout through api

---
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserAuthController extends Controller
+{
+
+    public function login(Request $request){
+        //required a valid email format and enough string for a password
+        $loginUserData = $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ]);
+
+        $user = User::where('email',$loginUserData['email'])->first();
+        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],401);
+        }
+        $user->tokens()->delete();
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            "message"=>"logged out"
+        ]);
+    }
+}
