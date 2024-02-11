3f74c468b7ff8a16f785e831faa3742e2dccebf4
JuanObiJuan <obi_juan_@hotmail.com>
2024-02-11 13:04:59 +0100

    seed some test users from a users.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $jsonPath = database_path() . '/seeders/testdata/' . 'users.json';
+        $usersArray = json_decode(file_get_contents($jsonPath), true);
+        foreach ($usersArray as $testUser) {
+            $dbUser = DB::table('users')->where('email', $testUser['email'])->first();
+
+            if (is_null($dbUser)) {
+                echo "creating user " . $testUser['name'];
+                User::create([
+                    'name' => $testUser['name'],
+                    'email' => $testUser['email'],
+                    'password' => $testUser['password'],
+                    'is_super_admin' => $testUser['is_super_admin'],
+                ]);
+            } else {
+                echo "already exist user " . $testUser['name'];
+            }
+            echo "\n";
+        }
     }
 }
