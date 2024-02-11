f4fff6c851c18de232f3055533fb4370ea92f565
JuanObiJuan <obi_juan_@hotmail.com>
2024-02-11 14:47:31 +0100

    Organizations testing data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Organization;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -21,23 +22,58 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        //USERS
         $jsonPath = database_path() . '/seeders/testdata/' . 'users.json';
         $usersArray = json_decode(file_get_contents($jsonPath), true);
-        foreach ($usersArray as $testUser) {
-            $dbUser = DB::table('users')->where('email', $testUser['email'])->first();
+        $testUsers=[];
+        foreach ($usersArray as $testItem) {
+            $dbUser = DB::table('users')->where('email', $testItem['email'])->first();
 
             if (is_null($dbUser)) {
-                echo "creating user " . $testUser['name'];
+                echo "creating user " . $testItem['name'];
                 User::create([
-                    'name' => $testUser['name'],
-                    'email' => $testUser['email'],
-                    'password' => $testUser['password'],
-                    'is_super_admin' => $testUser['is_super_admin'],
+                    'name' => $testItem['name'],
+                    'email' => $testItem['email'],
+                    'password' => $testItem['password'],
+                    'is_super_admin' => $testItem['is_super_admin'],
                 ]);
+
             } else {
-                echo "already exist user " . $testUser['name'];
+                echo "already exist user " . $testItem['name'];
             }
+
+            $dbUser = DB::table('users')->where('email', $testItem['email'])->first();
+            array_push($testUsers,$dbUser);
             echo "\n";
         }
+
+
+        //ORGANIZATIONS
+        $jsonPath = database_path() . '/seeders/testdata/' . 'organizations.json';
+        $organizationsTestArray = json_decode(file_get_contents($jsonPath), true);
+        $testOrganizations=[];
+
+        foreach ($organizationsTestArray as $testItem) {
+            $dbOrganization = DB::table('organizations')->where('email', $testItem['email'])->first();
+
+            if (is_null($dbOrganization)) {
+                echo "creating org " . $testItem['name'];
+                Organization::create([
+                    'name' => $testItem['name'],
+                    'email' => $testItem['email'],
+                    'website' => $testItem['website'],
+                    'cover_members_cost' => $testItem['cover_members_cost'],
+                    'allow_guests' => $testItem['allow_guests'],
+                    'cover_guests_cost' => $testItem['cover_guests_cost'],
+                ]);
+            } else {
+                echo "already exist item " . $testItem['name'];
+            }
+            $dbOrganization = DB::table('organizations')->where('email', $testItem['email'])->first();
+            array_push($testOrganizations,$dbOrganization);
+            echo "\n";
+        }
+
+
     }
 }
