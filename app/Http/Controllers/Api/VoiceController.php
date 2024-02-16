de0f901fa79bb34236ba9391c975ac22b263211a
JuanObiJuan <obi_juan_@hotmail.com>
2024-02-16 17:19:16 +0100

    voice controller

--- app/Http/Controllers/Api/VoiceController.php
+++ app/Http/Controllers/Api/VoiceController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\VoiceResource;
+use App\Models\Organization;
+use App\Models\Voice;
+use Illuminate\Support\Facades\Auth;
 
-class VoiceController extends Controller
+class SceneController extends Controller
 {
-    //
+    public function show($org_id, $app_id, $voice_id)
+    {
+        $voice = Voice::where('id', $voice_id)
+            ->whereHas('application', function ($query) use ($app_id) {
+                $query->where('id', $app_id);
+            })
+            ->whereHas('organization', function ($query) use ($org_id) {
+                $query->where('id', $org_id);
+            })
+            ->first();
+
+        //IF NOT FOUND ANSWER ACCORDINGLY
+        if (!$voice) return response()->json(['error' => 'Not found.'], 404);
+
+        //IF AUTHORIZED ANSWER ACCORDINGLY
+        $user = Auth::user();
+        $user_is_superAdmin = $user->isSuperAdmin();
+        $user_is_OrgAdmin = $user->isOrgAdmin($org_id);
+        $user_is_OrgManager = $user->isOrgManager($org_id);
+        $user_is_OrgMember = $user->isOrgMember($org_id);
+        $organization = Organization::find($org_id);
+        $user_is_allowed_As_guest = $organization->GuestAreAllowed();
+
+        if ($user_is_superAdmin
+            || $user_is_OrgAdmin
+            || $user_is_OrgManager
+            || $user_is_OrgMember
+            || $user_is_allowed_As_guest
+        ) return new VoiceResource($voice);
+
+        //WHEN NOT AUTHORIZED ANSWER ACCORDINGLY
+        return response()->json(['error' => 'Not authorized.'], 403);
+
+    }
 }
