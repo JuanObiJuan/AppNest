e88ec0a2e5da6b84cfb06ea503aa26581ccec02e
JuanObiJuan <obi_juan_@hotmail.com>
2024-03-09 14:37:52 +0100

    Form made with filament fields and saving to json succesfully

--- app/Filament/Resources/ApplicationResource.php
+++ app/Filament/Resources/ApplicationResource.php
@@ -4,9 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ApplicationResource\Pages;
 use App\Filament\Resources\ApplicationResource\RelationManagers;
-use App\Forms\Components\CustomJsonData;
 use App\Models\Application;
-use Creagia\FilamentCodeField\CodeField;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -15,7 +13,6 @@ use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Wiebenieuwenhuis\FilamentCodeEditor\Components\CodeEditor;
 
 class ApplicationResource extends Resource
 {
@@ -26,21 +23,94 @@ class ApplicationResource extends Resource
     protected static ?string $navigationGroup = 'Application Management';
     protected static ?int $navigationSort = 1;
 
+    public static function GetDynamicFields($jsonData, $jsonSchema, $jsonUiSchema):array {
+        $fields = [];
+
+        // Iterate over schema properties to create form fields
+        foreach ($jsonSchema['properties'] as $propertyKey => $propertyValue) {
+            $fieldType = $propertyValue['type'];
+            $uiSchema = $jsonUiSchema[$propertyKey] ?? null;
+
+            // Based on the type of field, create appropriate Filament form field
+            if ($fieldType === 'object' && $uiSchema) {
+                foreach ($propertyValue['properties'] as $langKey => $langValue) {
+                    $field=null;
+                    $uiOptions = $uiSchema['ui:options'][$langKey] ?? [];
+                    $label = $uiOptions['label'] ?? ucfirst($langKey);
+                    $componentType = $uiSchema['ui:component'];
+
+                    // Create field based on component type
+                    switch ($componentType) {
+                        case 'Textarea':
+                            $field = Forms\Components\Textarea::make("json_data.".$propertyKey.".".$langKey)
+                                ->label($label)
+                                ->placeholder($uiOptions['placeholder'] ?? '')
+                                ->rows($uiOptions['rows'] ?? 2)
+                                ->default($jsonData[$propertyKey][$langKey] ?? '');
+                            //echo(json_encode($jsonData[$propertyKey][$langKey]));
+
+                            break;
+
+                        case 'Toggle':
+                            $field = Forms\Components\Toggle::make("json_data.".$propertyKey.".".$langKey)
+                                ->label($label)
+                                ->default($jsonData[$propertyKey][$langKey] ?? false);
+                            break;
+
+                        // Add more cases as needed for different types of fields
+
+                        default:
+                            // Default to a simple TextInput if no specific component is matched
+                            $field = Forms\Components\TextInput::make("json_data.".$propertyKey.".".$langKey)
+                                ->label($label)
+                                ->default($jsonData[$propertyKey][$langKey] ?? '');
+                            break;
+                    }
+                    if ($field) {
+                        $fields[] = $field;
+                    }
+
+                    // Add the dynamically created field to the fields array
+                    //$fields[] = $field;
+                }
+            }
+        }
+
+        return $fields;
+    }
+
     public static function form(Form $form): Form
     {
         $record = $form->getRecord();
-        $json_data_array = json_decode($record->json_data , true);
+        //dd($record->json_data);
+        $json_data_array=[];
+        if (is_array($record->json_data)){
+            $json_data_array=$record->json_data;
+        }
+        else{
+            $json_data_array=json_decode($record->json_data , true);
+        }
         $json_schema_array = json_decode($record->json_schema , true);
         $json_admin_ui_schema_array = json_decode($record->json_admin_ui_schema , true);
 
-        return $form
-            ->schema([
+        $dynamicFields = self::GetDynamicFields($json_data_array, $json_schema_array, $json_admin_ui_schema_array);
+
+        return $form->schema(
+            array_merge([
                 Forms\Components\TextInput::make('name'),
-                CustomJsonData::make('json_data')
-                    ->jsonSchema($json_schema_array)
-                    ->jsonUiSchema($json_admin_ui_schema_array)
-                ]);
+            ], $dynamicFields)
+        );
+//        return $form
+//            ->schema([
+//                Forms\Components\TextInput::make('name'),
+//                CustomJsonData::make('json_data')
+//                    ->jsonSchema($json_schema_array)
+//                    ->jsonUiSchema($json_admin_ui_schema_array)
+//                ]);
     }
+
+
+
 
     public static function table(Table $table): Table
     {
